3bc8e72a4250703d5b45a2e498c4ad869b94a696
Ashwin Bordoloi <ashwin1014@yahoo.co.in>
2018-11-23 21:27:20 +0530

    add local storage check

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,90 @@
-var appDashBoardFunctions = (()=> {   
+window.onload = ()=> {
+    appDashBoardFunctions.displayTable();
+    appDashBoardFunctions.checkLocalStorage();
+}
+
+let appDashBoardFunctions = (()=> {   
     
-    // let membersArray = [
-    //     {name: 'Wayne Rooney', company: 'Manchester United', status: 'closed', lastUpdated: '7/07/2017', notes:'Highest scorer'},
-    //     {name: 'David Beckham', company: 'Manchester United', status: 'closed', lastUpdated: '3/08/2003', notes:'Most stylish player'},
-    // ]
+    let membersArray = [
+        {name: 'Wayne Rooney', company: 'Manchester United', status: 'closed', lastUpdated: '7/07/2017', notes:'Highest scorer'},
+        {name: 'David Beckham', company: 'Manchester United', status: 'closed', lastUpdated: '3/08/2003', notes:'Most stylish player'},
+        {name: 'Ryan Giggs', company: 'Manchester United', status: 'closed', lastUpdated: '3/08/2011', notes:'Most matches played'},
+        {name: 'Paul Pogba', company: 'Manchester United', status: 'Active', lastUpdated: '3/08/2011', notes:'United\'s most expensive'}
+    ];
+
+    const checkLocalStorage = ()=> {
+        if (localStorage.getItem("members") === null) {
+            localStorage.setItem("members", JSON.stringify(membersArray))
+        }
+    }
+
+    const saveItems = ()=> {
+        let saveObject = {
+            name: document.getElementById('memberName').value, 
+            company: document.getElementById('memberCompany').value, 
+            status: document.getElementById('memberStatus').value, 
+            lastUpdated: document.getElementById('memberLastUpdate').value, 
+            notes:document.getElementById('memberNotes').value
+        };
+        membersArray.push(saveObject)
+        localStorage.setItem("members", JSON.stringify(membersArray));
+        closeModal()
+    }
+
+    const generateMembersTable = ()=> {
+        
+    }
   
-    let openModal = ()=> {
+    const openModal = ()=> {
         document.getElementById('addMemberPopup').classList.add('open');
     };
 
-    let closeModal = ()=> {
+    const closeModal = ()=> {
         document.getElementById('addMemberPopup').classList.remove('open');
     };
 
+     const toggleCheckbox = (ele)=> {
+        let checkbox = document.querySelectorAll('.chkbox');
+        checkbox.forEach((item)=>{
+            if(item !== ele) item.checked = ele.checked
+       });
+    };
+
+    const deleteRow = (ele) => {
+        ele.parentElement.remove();
+    };
+
+    let displayTable = () => {
+     //   console.log(membersArray);
+    }
+
+
 
     //reveal functions
-    var returnObject= {
+    const returnObject = {
         openModal: openModal,
-        closeModal: closeModal
+        closeModal: closeModal,
+        toggleCheckbox: toggleCheckbox,
+        deleteRow: deleteRow,
+        saveItems: saveItems,
+        displayTable: displayTable,
+        checkLocalStorage: checkLocalStorage
     }
 
    return returnObject;
 
 })();
 
-   
-
-// window.addEventListener('load', function() {
-
-// }, false)
 
 document.getElementById('addMembers').addEventListener('click', function() {
     appDashBoardFunctions.openModal();
 });
+
+document.getElementById('btnSave').addEventListener('click', function(){
+    appDashBoardFunctions.saveItems();
+})
+
+
 
 // document.getElementById('add').addEventListener('click', function() {
 //     // newItems.push({name: 'Ryan Giggs', company: 'Manchester United', status: 'closed', lastUpdated: '3/08/2011', notes:'Most matches played'})
